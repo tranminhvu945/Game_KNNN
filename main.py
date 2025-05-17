5766524f1cf4b351830d59867e4a4858780f115c
helloMinh <phandinhminh48@gmail.com>
2025-05-17 15:31:52 +0700

    done

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 from src.game import Game
-import os
-import sys
-import pygame
-
-def main():
-    if sys.platform.startswith('win'):
-        try:
-            import ctypes
-            ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 6)
-        except:
-            pass
-    
-    game = Game()
-    game.run()
 
 if __name__ == "__main__":
-    main()
+    game = Game()
+    game.run()
